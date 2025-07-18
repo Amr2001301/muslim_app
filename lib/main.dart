4d30b1bdc46bd81d9fd1544c8bae3c4c5a33cbdf
amr <amrrakha20001@gmail.com>
2025-07-18 17:14:09 +0300

    add easy localization add change localization bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:muslim_app/muslim_app.dart';
@@ -5,11 +6,18 @@ import 'package:path_provider/path_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await EasyLocalization.ensureInitialized();
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: HydratedStorageDirectory(
       (await getTemporaryDirectory()).path,
     ),
   );
-  runApp(const MuslimApp());
+  runApp(
+    EasyLocalization(
+      supportedLocales: [Locale('en'), Locale('ar')],
+      path: 'assets/translations',
+      fallbackLocale: Locale('ar'),
+      child: const MuslimApp(),
+    ),
+  );
 }
--- lib/muslim_app.dart
+++ lib/muslim_app.dart
@@ -1,9 +1,10 @@
-import 'dart:developer';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:muslim_app/core/theme/bloc/theme_bloc.dart';
 import 'package:muslim_app/core/theme/dark_theme.dart';
 import 'package:muslim_app/core/theme/light_theme.dart';
+import 'package:muslim_app/core/translations/bloc/translations_bloc.dart';
 
 class MuslimApp extends StatelessWidget {
   const MuslimApp({super.key});
@@ -11,26 +12,19 @@ class MuslimApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: [BlocProvider(create: (context) => ThemeBloc())],
+      providers: [
+        BlocProvider(create: (context) => ThemeBloc()),
+        BlocProvider(create: (context) => TranslationsBloc()),
+      ],
       child: BlocBuilder<ThemeBloc, ThemeState>(
         builder: (context, state) {
           return MaterialApp(
+            localizationsDelegates: context.localizationDelegates,
+            supportedLocales: context.supportedLocales,
+            locale: context.locale,
             theme: state.isDarkMode ? darkTheme() : lightTheme(),
             debugShowCheckedModeBanner: false,
-
-            home: Scaffold(
-              body: Center(
-                child: TextButton(
-                  onPressed: () {
-                    context.read<ThemeBloc>().add(
-                      ChangeTheme(isDark: !state.isDarkMode),
-                    );
-                    log(state.isDarkMode.toString());
-                  },
-                  child: Text("Change theme"),
-                ),
-              ),
-            ),
+            home: Scaffold(body: Container()),
           );
         },
       ),
